9fc7caedffe1307107a326613bae1dbe4a0b4404
Alberto <atgg@gft.com>
2023-10-06 10:02:05 +0200

    ADD PaymentMethod entity + Create OneToMany relationship

---
+++ src/main/java/com/gfttraining/users/model/User.java
@@ -0,0 +1,26 @@
+package com.gfttraining.users.model;
+
+import com.gfttraining.users.model.PaymentMethod;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private String address;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "payment_method_id")
+    private PaymentMethod paymentMethod;
+    private int fidelityPoints;
+    private double averagePurchase;
+}
