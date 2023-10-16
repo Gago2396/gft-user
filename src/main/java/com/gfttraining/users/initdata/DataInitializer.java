9ac647a0a4ce40457976eeb3beec2832ed72fa82
Rafael Nieto Soto <radikan.dev@gmail.com>
2023-10-16 16:00:46 +0200

    ADD more objects to initialization

--- src/main/java/com/gfttraining/users/initdata/DataInitializer.java
+++ src/main/java/com/gfttraining/users/initdata/DataInitializer.java
@@ -28,7 +28,7 @@ public class DataInitializer implements CommandLineRunner {
     private void initializeData() {
         PaymentMethod paymentMethod1 = new PaymentMethod(1L, "Credit Card");
         PaymentMethod paymentMethod2 = new PaymentMethod(2L, "PayPal");
-        PaymentMethod paymentMethod3 = new PaymentMethod(3L, "Paysafecard");
+        PaymentMethod paymentMethod3 = new PaymentMethod(3L, "Stripe");
 
         paymentMethodRepository.save(paymentMethod1);
         paymentMethodRepository.save(paymentMethod2);
@@ -40,6 +40,11 @@ public class DataInitializer implements CommandLineRunner {
         users.add(new User(3L, "Michael", "Johnson", "789 Main St", paymentMethod1, 120, 90.75));
         users.add(new User(4L, "Emily", "Davis", "456 Pine St", paymentMethod2, 200, 120.30));
         users.add(new User(5L, "David", "Anderson", "1010 Maple St", paymentMethod3, 80, 60.00));
+        users.add(new User(6L, "Sarah", "Johnson", "2222 Oak St", paymentMethod3, 90, 80.00));
+        users.add(new User(7L, "Mark", "Davis", "3333 Elm St", paymentMethod3, 120, 95.50));
+        users.add(new User(8L, "Anna", "Smith", "4444 Pine St", paymentMethod1, 180, 150.75));
+        users.add(new User(9L, "Michael", "Wilson", "5555 Maple St", paymentMethod2, 140, 110.20));
+        users.add(new User(10L, "Jennifer", "Brown", "6666 Cedar St", paymentMethod1, 160, 120.00));
 
         userRepository.saveAll(users);
     }
