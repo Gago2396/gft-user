f7c81e156c09f99e6d0c736f3da8eb0dc81bbf72
Rafael Nieto Soto <radikan.dev@gmail.com>
2023-10-16 15:55:42 +0200

    ADD another paymentMethod to initialization

--- src/main/java/com/gfttraining/users/initdata/DataInitializer.java
+++ src/main/java/com/gfttraining/users/initdata/DataInitializer.java
@@ -28,16 +28,18 @@ public class DataInitializer implements CommandLineRunner {
     private void initializeData() {
         PaymentMethod paymentMethod1 = new PaymentMethod(1L, "Credit Card");
         PaymentMethod paymentMethod2 = new PaymentMethod(2L, "PayPal");
+        PaymentMethod paymentMethod3 = new PaymentMethod(3L, "Paysafecard");
 
         paymentMethodRepository.save(paymentMethod1);
         paymentMethodRepository.save(paymentMethod2);
+        paymentMethodRepository.save(paymentMethod3);
 
         List<User> users = new ArrayList<>();
         users.add(new User(1L, "John", "Doe", "1234 Elm St", paymentMethod1, 100, 75.50));
         users.add(new User(2L, "Jane", "Smith", "5678 Oak St", paymentMethod2, 150, 100.20));
         users.add(new User(3L, "Michael", "Johnson", "789 Main St", paymentMethod1, 120, 90.75));
         users.add(new User(4L, "Emily", "Davis", "456 Pine St", paymentMethod2, 200, 120.30));
-        users.add(new User(5L, "David", "Anderson", "1010 Maple St", paymentMethod1, 80, 60.00));
+        users.add(new User(5L, "David", "Anderson", "1010 Maple St", paymentMethod3, 80, 60.00));
 
         userRepository.saveAll(users);
     }
