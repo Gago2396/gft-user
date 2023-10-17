4e6db6205e6220bbd8b02f7f02acde10d5fe2ad3
Alberto Gago <albertogagoprogramacion@gmail.com>
2023-10-17 17:28:29 +0200

    ADD deleteFavorite + REFACTOR args in Favorite and FavoriteRequest

--- src/main/java/com/gfttraining/users/services/FavoriteService.java
+++ src/main/java/com/gfttraining/users/services/FavoriteService.java
@@ -1,6 +1,7 @@
 package com.gfttraining.users.services;
 
 import com.gfttraining.users.models.Favorite;
+import com.gfttraining.users.models.FavoritePK;
 import com.gfttraining.users.repositories.FavoriteRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class FavoriteService {
 
     public Favorite addFavorite(Favorite favorite) {
         return favoriteRepository.save(favorite);
+    }
+
+    public void deleteFavorite(Long user, Long product) {
+        FavoritePK id = new FavoritePK(user, product);
+        favoriteRepository.deleteById(id);
     }
 }
