7bc0fd2f892ea8978edee9f479f48a821f502ac7
Rafael <radikan.dev@gmail.com>
2023-10-20 09:58:30 +0200

    WIP UserService tests

--- src/test/java/com/gfttraining/users/controllers/FavoriteControllerTest.java
+++ src/test/java/com/gfttraining/users/controllers/FavoriteControllerTest.java
@@ -8,6 +8,7 @@ import com.gfttraining.users.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,84 +19,84 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+// ToDo: finish this test when connection with microservice is finished
 @ExtendWith(MockitoExtension.class)
 class FavoriteControllerTest {
 
-    @InjectMocks
-    private FavoriteController favoriteController;
-
-    @Mock
-    private FavoriteService favoriteService;
-
-    @Mock
-    private UserService userService;
-
-    private FavoriteRequest testFavoriteRequest;
-    private User testUser;
-
-    @BeforeEach
-    void setUp() {
-        // Mock User
-        testUser = new User();
-        testUser.setId(1L);
-
-        // FavoriteRequest
-        testFavoriteRequest = new FavoriteRequest();
-        testFavoriteRequest.setUser(1L);
-        testFavoriteRequest.setProduct("SampleProduct");
-    }
-
-    @Test
-    @DisplayName("Add Favorite")
-    void testAddFavorite() {
-        when(userService.getUserById(testFavoriteRequest.getUser())).thenReturn(testUser);
-        Favorite favorite = new Favorite(testUser, testFavoriteRequest.getProduct());
-        when(favoriteService.addFavorite(favorite)).thenReturn(favorite);
-
-        ResponseEntity<?> responseEntity = favoriteController.addFavorite(testFavoriteRequest);
-
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-        assertEquals(favorite, responseEntity.getBody());
-        verify(userService, times(1)).getUserById(testFavoriteRequest.getUser());
-        verify(favoriteService, times(1)).addFavorite(favorite);
-    }
-
-    @Test
-    @DisplayName("Delete Favorite")
-    void testDeleteFavorite() {
-        doNothing().when(favoriteService).deleteFavorite(testFavoriteRequest.getUser(), testFavoriteRequest.getProduct());
-
-        ResponseEntity<String> responseEntity = favoriteController.deleteFavorite(testFavoriteRequest);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("Favorite deleted successfully", responseEntity.getBody());
-        verify(favoriteService, times(1)).deleteFavorite(testFavoriteRequest.getUser(), testFavoriteRequest.getProduct());
-    }
-
-    @Test
-    @DisplayName("Search User Favorites")
-    void testSearchUserFavorites() {
-        long userId = 1L;
-        when(favoriteService.searchUserFavorites(userId)).thenReturn(Arrays.asList(testFavoriteRequest.getProduct()));
-
-        ResponseEntity<?> responseEntity = favoriteController.searchUserFavorites(userId);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(Arrays.asList(testFavoriteRequest.getProduct()), responseEntity.getBody());
-        verify(favoriteService, times(1)).searchUserFavorites(userId);
-    }
-
-    @Test
-    @DisplayName("Delete Favorite by Product")
-    void testDeleteFavoriteByProduct() {
-        long productId = 1L;
-        doNothing().when(favoriteService).deleteFavoriteByProduct(productId);
-
-        ResponseEntity<String> responseEntity = favoriteController.deleteFavoriteByProduct(productId);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("Favorite deleted successfully", responseEntity.getBody());
-        verify(favoriteService, times(1)).deleteFavoriteByProduct(productId);
-    }
+//    @InjectMocks
+//    private FavoriteController favoriteController;
+//
+//    @Mock
+//    private FavoriteService favoriteService;
+//
+//    @Mock
+//    private UserService userService;
+//
+//    private FavoriteRequest testFavoriteRequest;
+//    private User testUser;
+//
+//    @BeforeEach
+//    void setUp() {
+//        // Mock User
+//        testUser = new User();
+//        testUser.setId(1L);
+//
+//        // FavoriteRequest
+//        testFavoriteRequest = new FavoriteRequest();
+//        testFavoriteRequest.setUser(1L);
+//        testFavoriteRequest.setProduct("SampleProduct");
+//    }
+//
+//    @Test
+//    @DisplayName("Add Favorite")
+//    void testAddFavorite() {
+//        when(userService.getUserById(testFavoriteRequest.getUser())).thenReturn(testUser);
+//        Favorite favorite = new Favorite(testUser, testFavoriteRequest.getProduct());
+//        when(favoriteService.addFavorite(favorite)).thenReturn(favorite);
+//
+//        ResponseEntity<?> responseEntity = favoriteController.addFavorite(testFavoriteRequest);
+//
+//        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+//        assertEquals(favorite, responseEntity.getBody());
+//        verify(userService, times(1)).getUserById(testFavoriteRequest.getUser());
+//        verify(favoriteService, times(1)).addFavorite(favorite);
+//    }
+//
+//    @Test
+//    @DisplayName("Delete Favorite")
+//    void testDeleteFavorite() {
+//        doNothing().when(favoriteService).deleteFavorite(testFavoriteRequest.getUser(), testFavoriteRequest.getProduct());
+//
+//        ResponseEntity<String> responseEntity = favoriteController.deleteFavorite(testFavoriteRequest);
+//
+//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+//        assertEquals("Favorite deleted successfully", responseEntity.getBody());
+//        verify(favoriteService, times(1)).deleteFavorite(testFavoriteRequest.getUser(), testFavoriteRequest.getProduct());
+//    }
+//
+//    @Test
+//    @DisplayName("Search User Favorites")
+//    void testSearchUserFavorites() {
+//        long userId = 1L;
+//        when(favoriteService.searchUserFavorites(userId)).thenReturn(Arrays.asList(testFavoriteRequest.getProduct()));
+//
+//        ResponseEntity<?> responseEntity = favoriteController.searchUserFavorites(userId);
+//
+//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+//        assertEquals(Arrays.asList(testFavoriteRequest.getProduct()), responseEntity.getBody());
+//        verify(favoriteService, times(1)).searchUserFavorites(userId);
+//    }
+//
+//    @Test
+//    @DisplayName("Delete Favorite by Product")
+//    void testDeleteFavoriteByProduct() {
+//        long productId = 1L;
+//        doNothing().when(favoriteService).deleteFavoriteByProduct(productId);
+//
+//        ResponseEntity<String> responseEntity = favoriteController.deleteFavoriteByProduct(productId);
+//
+//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+//        assertEquals("Favorite deleted successfully", responseEntity.getBody());
+//        verify(favoriteService, times(1)).deleteFavoriteByProduct(productId);
+//    }
 }
---
+++ src/test/java/com/gfttraining/users/services/UserServiceTest.java
@@ -0,0 +1,213 @@
+package com.gfttraining.users.services;
+
+import com.gfttraining.users.controllers.UserController;
+import com.gfttraining.users.models.*;
+import com.gfttraining.users.repositories.FavoriteRepository;
+import com.gfttraining.users.repositories.PaymentMethodRepository;
+import com.gfttraining.users.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserController userController;
+
+    @Mock
+    private PaymentMethodService paymentMethodService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private FavoriteRepository favoriteRepository;
+
+    private UserRequest testUserRequest;
+
+    private UserRequest updatedUserRequest;
+
+    private User testUser;
+
+    private User updatedTestUser;
+
+    private Country updatedCountry;
+
+    private Address updatedAddress;
+
+    private PaymentMethod paymentMethod;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController(userService);
+
+        // PaymentMethod
+        paymentMethod = new PaymentMethod();
+        paymentMethod.setId(1L);
+        paymentMethod.setName("PayPal");
+
+        // Country
+        Country country = new Country();
+        country.setName("Spain");
+
+        // Updated Country
+        updatedCountry = new Country();
+        country.setName("Estonia");
+
+        // Address
+        Address address = new Address();
+        address.setStreet("23 Mayor");
+        address.setCity("Valencia");
+        address.setProvince("Valencia");
+        address.setPostalCode(46002);
+        address.setCountry(country);
+
+        // Updated Address
+        updatedAddress = new Address();
+        address.setStreet("23 Mayor Updated");
+        address.setCity("Valencia Updated");
+        address.setProvince("Valencia Updated");
+        address.setPostalCode(46022);
+        address.setCountry(updatedCountry);
+
+        // User
+        testUser = new User();
+        testUser.setName("Antonio");
+        testUser.setLastName("Garcia");
+        testUser.setAddress(address);
+        testUser.setPaymentMethod(paymentMethod);
+        testUser.setFidelityPoints(300);
+        testUser.setAveragePurchase(120.0);
+
+        // Updated User
+        updatedTestUser = new User();
+        updatedTestUser.setName("Antonio Updated");
+        updatedTestUser.setLastName("Garcia Updated");
+        updatedTestUser.setAddress(address);
+        updatedTestUser.setPaymentMethod(paymentMethod);
+        updatedTestUser.setFidelityPoints(300);
+        updatedTestUser.setAveragePurchase(120.0);
+
+        // UserRequest
+        UserRequest userRequest = new UserRequest();
+        userRequest.setName("Antonio");
+        userRequest.setLastName("Garcia");
+        userRequest.setStreet("23 Mayor");
+        userRequest.setCity("Valencia");
+        userRequest.setProvince("Valencia");
+        userRequest.setPostalCode(46002);
+        userRequest.setCountry("Spain");
+        userRequest.setPaymentMethod("Credit Card");
+        userRequest.setFidelityPoints(300);
+        userRequest.setAveragePurchase(120.0);
+
+        // Updated UserRequest
+        updatedUserRequest = new UserRequest();
+        updatedUserRequest.setName("Alexelcapo");
+        updatedUserRequest.setLastName("Jimeno");
+        updatedUserRequest.setStreet("45 Menor");
+        updatedUserRequest.setCity("Madrid");
+        updatedUserRequest.setProvince("Madrid");
+        updatedUserRequest.setPostalCode(46123);
+        updatedUserRequest.setCountry("Estonia");
+        updatedUserRequest.setPaymentMethod("Paypal");
+        updatedUserRequest.setFidelityPoints(400);
+        updatedUserRequest.setAveragePurchase(150.5);
+    }
+
+    @Test
+    void findPaymentMethod() {
+        String paymentMethodName = paymentMethod.getName();
+
+        when(paymentMethodService.getPaymentMethodByName(paymentMethodName)).thenReturn(Optional.ofNullable(paymentMethod));
+
+        Optional<PaymentMethod> result = paymentMethodService.getPaymentMethodByName(paymentMethodName);
+
+        assertThat(result.get().getName(), equalTo("PayPal"));
+        assertThat(result.get().getId(), equalTo(1L));
+
+        assertThat(result.get(), equalTo(paymentMethod));
+
+        verify(paymentMethodService, times(1)).getPaymentMethodByName(paymentMethodName);
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+        Long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(testUser));
+
+        doNothing().when(favoriteRepository).deleteByUser(testUser);
+
+        userService.deleteUserById(userId);
+
+        verify(userRepository, times(1)).findById(userId);
+
+        verify(favoriteRepository, times(1)).deleteByUser(testUser);
+
+        verify(userRepository, times(1)).deleteById(userId);
+    }
+
+    @Test
+    void updateUserById() {
+        long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(testUser));
+
+        when(userService.parseUser(updatedUserRequest)).thenReturn(updatedTestUser);
+
+        when(userRepository.save(updatedTestUser)).thenReturn(updatedTestUser);
+
+        User result = userService.updateUserById(userId, updatedUserRequest);
+
+        verify(userRepository, times(1)).findById(userId);
+
+        verify(userService, times(1)).parseUser(updatedUserRequest);
+
+        verify(userRepository, times(1)).save(updatedTestUser);
+
+        assertEquals(updatedTestUser, result);
+    }
+
+
+    @Test
+    void parseUser() {
+    }
+
+    @Test
+    void loadListOfUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getUserByName() {
+    }
+
+    @Test
+    void getListOfUsers() {
+    }
+}
