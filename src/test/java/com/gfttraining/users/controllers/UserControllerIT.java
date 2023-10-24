5131c584e320de0cf0cc56139953ba56a26a97a9
carlac <carlacaldes@me.com>
2023-10-24 17:15:15 +0200

    ADD some new IT still WIP v2. New error DNW in createUserTest

--- src/test/java/com/gfttraining/users/controllers/UserControllerIT.java
+++ src/test/java/com/gfttraining/users/controllers/UserControllerIT.java
@@ -4,6 +4,7 @@ import com.gfttraining.users.models.PaymentMethod;
 import com.gfttraining.users.models.User;
 import com.gfttraining.users.models.UserRequest;
 import com.gfttraining.users.services.FavoriteService;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -63,14 +64,14 @@ public class UserControllerIT {
     void createUserTest() {
         UserRequest userRequest = new UserRequest(
                 "John",
-                "Doe",
+                "Lennon",
                 "123 Main St",
-                "City",
-                "Province",
-                12345,
-                "Spain",
+                "Liverpool",
+                "Merseyside",
+                13456,
+                "United Kingdom",
                 "PayPal",
-                100,
+                170,
                 75.0
         );
 
@@ -81,14 +82,14 @@ public class UserControllerIT {
                 .expectStatus().isCreated()
                 .expectBody()
                 .jsonPath("$.name").isEqualTo("John")
-                .jsonPath("$.lastName").isEqualTo("Doe")
+                .jsonPath("$.lastName").isEqualTo("Lennon")
                 .jsonPath("$.address.street").isEqualTo("123 Main St")
-                .jsonPath("$.address.city").isEqualTo("City")
-                .jsonPath("$.address.province").isEqualTo("Province")
-                .jsonPath("$.address.postalCode").isEqualTo(12345)
-                .jsonPath("$.address.country.name").isEqualTo("Spain")
+                .jsonPath("$.address.city").isEqualTo("Liverpool")
+                .jsonPath("$.address.province").isEqualTo("Merseyside")
+                .jsonPath("$.address.postalCode").isEqualTo(13456)
+                .jsonPath("$.address.country.name").isEqualTo("United Kingdom")
                 .jsonPath("$.paymentMethod.name").isEqualTo("PayPal")
-                .jsonPath("$.fidelityPoints").isEqualTo(100)
+                .jsonPath("$.fidelityPoints").isEqualTo(170)
                 .jsonPath("$.averagePurchase").isEqualTo(75.0);
     }
 
@@ -98,14 +99,14 @@ public class UserControllerIT {
     void postCreateUserTestNull() {
         UserRequest userRequest = new UserRequest(
                 null,
-                "Doe",
+                "Lennon",
                 "123 Main St",
-                "City",
-                "Province",
-                12345,
-                "Spain",
+                "Liverpool",
+                "Merseyside",
+                13456,
+                "United Kingdom",
                 "PayPal",
-                100,
+                170,
                 75.0
         );
 
@@ -118,32 +119,42 @@ public class UserControllerIT {
                 .jsonPath("$").isEqualTo("Validation error(s):\n Name is required;");
     }
 
-    /*@Test
-    @DisplayName("Create User IT with Invalid Data - Bad Request")
+    @Test
+    @DisplayName("Create User IT with Invalid Data - Bad Request: Name is required")
     @Order(3)
     void postCreateUserTestInvalid() {
-        UserRequest userRequest = new UserRequest(
-                "John",
-                "Doe",
-                "123 Main St",
-                "City",
-                "Province",
-                12345,
-                "Spain",
-                "PayPal",
-                -1,
-                75.0
-        );
+        JSONObject userRequest = new JSONObject();
+        userRequest.put("lastName", "Lennon");
+
+        JSONObject address = new JSONObject();
+        address.put("street", "123 Main St");
+        address.put("city", "Liverpool");
+        address.put("province", "Merseyside");
+        address.put("postalCode", 13456);
+
+        JSONObject country = new JSONObject();
+        country.put("name", "United Kingdom");
+
+        address.put("country", country);
+
+        userRequest.put("address", address);
+        userRequest.put("paymentMethod", "PayPal");
+        userRequest.put("fidelityPoints", 170);
+        userRequest.put("averagePurchase", 75.0);
+
+        String userRequestJson = userRequest.toString();
 
         client.post().uri("/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(userRequest)
+                .bodyValue(userRequestJson)
                 .exchange()
                 .expectStatus().isBadRequest()
-                .expectBody().jsonPath("$").isEqualTo("Validation error(s):\n Fidelity Points must be greater than or equal to 0;");
-    }*/
+                .expectBody()
+                .jsonPath("$").isEqualTo("Validation error(s):\n" +
+                        " Name is required;");
+    }
 
-    /*@Test
+    @Test
     @DisplayName("Get List of Users IT")
     @Order(4)
     void getListOfUsersTest() {
@@ -153,19 +164,29 @@ public class UserControllerIT {
                 .expectBody()
                 .jsonPath("$[0].name").isEqualTo("John")
                 .jsonPath("$[0].lastName").isEqualTo("Doe")
-                .jsonPath("$[0].address.street").isEqualTo("123 Main St")
-                .jsonPath("$[0].address.city").isEqualTo("City")
-                .jsonPath("$[0].address.province").isEqualTo("Province")
+                .jsonPath("$[0].address.street").isEqualTo("Sunset Blvd")
+                .jsonPath("$[0].address.city").isEqualTo("Barcelona")
+                .jsonPath("$[0].address.province").isEqualTo("Catalonia")
                 .jsonPath("$[0].address.postalCode").isEqualTo(12345)
                 .jsonPath("$[0].address.country.name").isEqualTo("Spain")
-                .jsonPath("$[0].paymentMethod.name").isEqualTo("PayPal")
+                .jsonPath("$[0].paymentMethod.name").isEqualTo("Credit Card")
                 .jsonPath("$[0].fidelityPoints").isEqualTo(100)
-                .jsonPath("$[0].averagePurchase").isEqualTo(75.0);
-    }*/
+                .jsonPath("$[0].averagePurchase").isEqualTo(75.50)
+                .jsonPath("$[1].name").isEqualTo("Jane")
+                .jsonPath("$[1].lastName").isEqualTo("Smith")
+                .jsonPath("$[1].address.street").isEqualTo("Green Valley Rd")
+                .jsonPath("$[1].address.city").isEqualTo("Tallinn")
+                .jsonPath("$[1].address.province").isEqualTo("Harju County")
+                .jsonPath("$[1].address.postalCode").isEqualTo(56789)
+                .jsonPath("$[1].address.country.name").isEqualTo("Estonia")
+                .jsonPath("$[1].paymentMethod.name").isEqualTo("PayPal")
+                .jsonPath("$[1].fidelityPoints").isEqualTo(150)
+                .jsonPath("$[1].averagePurchase").isEqualTo(100.20);
+    }
 
     @Test
     @DisplayName("Get User by ID IT")
-    @Order(6)
+    @Order(5)
     void getUserByIdTest() {
         client.get().uri("/users/1")
                 .exchange()
@@ -186,5 +207,40 @@ public class UserControllerIT {
                 .jsonPath("$.fidelityPoints").isEqualTo(100)
                 .jsonPath("$.averagePurchase").isEqualTo(75.50);
     }
+
+    @Test
+    @DisplayName("Get List of Users by Name IT")
+    @Order(6)
+    void getListOfUsersByNameTest() {
+        String nameToFilter = "John";
+        client.get().uri("/users/list?name=" + nameToFilter)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$[0].name").isEqualTo("John")
+                .jsonPath("$[0].lastName").isEqualTo("Doe")
+                .jsonPath("$[0].address.street").isEqualTo("Sunset Blvd")
+                .jsonPath("$[0].address.city").isEqualTo("Barcelona")
+                .jsonPath("$[0].address.province").isEqualTo("Catalonia")
+                .jsonPath("$[0].address.postalCode").isEqualTo(12345)
+                .jsonPath("$[0].address.country.name").isEqualTo("Spain")
+                .jsonPath("$[0].paymentMethod.name").isEqualTo("Credit Card")
+                .jsonPath("$[0].fidelityPoints").isEqualTo(100)
+                .jsonPath("$[0].averagePurchase").isEqualTo(75.50)
+                .jsonPath("$[10].name").isEqualTo("John")
+                .jsonPath("$[10].lastName").isEqualTo("Lennon")
+                .jsonPath("$[10].address.street").isEqualTo("123 Main St")
+                .jsonPath("$[10].address.city").isEqualTo("Liverpool")
+                .jsonPath("$[10].address.province").isEqualTo("Merseyside")
+                .jsonPath("$[10].address.postalCode").isEqualTo(13456)
+                .jsonPath("$[10].address.country.name").isEqualTo("United Kingdom")
+                .jsonPath("$[10].paymentMethod.name").isEqualTo("PayPal")
+                .jsonPath("$[10].fidelityPoints").isEqualTo(170)
+                .jsonPath("$[10].averagePurchase").isEqualTo(75.0)
+                .jsonPath("$[99].name").doesNotExist();
+    }
+
+
+
 
 }
