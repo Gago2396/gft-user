332d9187b5d4a2cfd25c8dbdfcd28959c3af3802
Rafael <radikan.dev@gmail.com>
2023-10-26 16:08:55 +0200

    ADD DisplayName

--- src/test/java/com/gfttraining/users/controllers/UserControllerAdviceTest.java
+++ src/test/java/com/gfttraining/users/controllers/UserControllerAdviceTest.java
@@ -3,6 +3,7 @@ package com.gfttraining.users.controllers;
 import com.gfttraining.users.exceptions.CartConnectionRefusedException;
 import com.gfttraining.users.exceptions.CartResponseFailedException;
 import com.gfttraining.users.exceptions.CountryNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -25,13 +26,12 @@ public class UserControllerAdviceTest {
     private UserController userController;
 
     @Test
+    @DisplayName("GIVEN ConstraintViolationException WHEN handleConstraintViolationException THEN return BAD_REQUEST response")
     public void testHandleConstraintViolationException() {
         UserControllerAdvice advice = new UserControllerAdvice();
 
-        // Crea una instancia de ConstraintViolationException con al menos una violación de restricción
         ConstraintViolationException ex = new ConstraintViolationException("Mensaje de excepción", new HashSet<>());
 
-        // Agrega una violación de restricción (en este caso, una violación de restricción de ejemplo)
         ConstraintViolation<?> constraintViolation = mock(ConstraintViolation.class);
         when(constraintViolation.getMessage()).thenReturn("La restricción no se cumplió");
         ex.getConstraintViolations().add(constraintViolation);
@@ -41,8 +41,8 @@ public class UserControllerAdviceTest {
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
-
     @Test
+    @DisplayName("GIVEN MethodArgumentNotValidException WHEN handleMethodArgumentNotValidException THEN return BAD_REQUEST response")
     public void testHandleMethodArgumentNotValidException() {
         UserControllerAdvice advice = new UserControllerAdvice();
         MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
@@ -53,6 +53,7 @@ public class UserControllerAdviceTest {
     }
 
     @Test
+    @DisplayName("GIVEN NoSuchElementException WHEN handleNoSuchElementException THEN return NOT_FOUND response")
     public void testHandleNoSuchElementException() {
         UserControllerAdvice advice = new UserControllerAdvice();
         NoSuchElementException ex = mock(NoSuchElementException.class);
@@ -63,6 +64,7 @@ public class UserControllerAdviceTest {
     }
 
     @Test
+    @DisplayName("GIVEN CartConnectionRefusedException WHEN handleCartConnectionRefusedException THEN return INTERNAL_SERVER_ERROR response")
     public void testCartConnectionRefusedException() {
         UserControllerAdvice advice = new UserControllerAdvice();
         CartConnectionRefusedException ex = mock(CartConnectionRefusedException.class);
@@ -73,6 +75,7 @@ public class UserControllerAdviceTest {
     }
 
     @Test
+    @DisplayName("GIVEN CountryNotFoundException WHEN handleCountryNotFoundException THEN return NOT_FOUND response")
     public void testHandleCountryNotFoundException() {
         UserControllerAdvice advice = new UserControllerAdvice();
         CountryNotFoundException ex = mock(CountryNotFoundException.class);
@@ -83,6 +86,7 @@ public class UserControllerAdviceTest {
     }
 
     @Test
+    @DisplayName("GIVEN CartResponseFailedException WHEN handleCartResponseFailedException THEN return BAD_REQUEST response")
     public void testCartResponseFailedException() {
         UserControllerAdvice advice = new UserControllerAdvice();
         CartResponseFailedException ex = mock(CartResponseFailedException.class);
